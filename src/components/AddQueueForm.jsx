ac7b793384f7d00a8079e6a8311d9d2a0c983493
Biswaranjan Pradhan <biswaranjan.pradhan@fiserv.com>
2026-09-25 12:48:34 +0530

    Added tailwindcss and styles to the components

--- src/components/AddQueueForm.jsx
+++ src/components/AddQueueForm.jsx
@@ -1,37 +1,47 @@
 import { useState } from "react";
 
-const AddQueueForm = ({onAdd}) => {
+const AddQueueForm = ({ onAdd }) => {
+  const [customer, setCustomer] = useState("");
+  const [service, setService] = useState("");
 
-    const [customer, setCustomer] = useState('');
-    const [service, setService] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!customer.trim() || !service.trim()) return;
+    onAdd({ customer, service });
+    setCustomer("");
+    setService("");
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(!customer.trim() || !service.trim()) return;
-        onAdd({customer, service});
-        setCustomer('');
-        setService('');
-    }
-
-    return (
-        <>           
-            <div>Queue Form</div>
-            <form onSubmit={handleSubmit}>
-                <input 
-                    type="input" 
-                    value={customer} 
-                    onChange={(e) => setCustomer(e.target.value)} 
-                    placeholder="Customer Name" />
-                <select value={service} onChange={(e) => setService(e.target.value)}>
-                    <option value=''>Select Service</option>
-                    <option value='consultation'>Consultation</option>
-                    <option value='payment'>Payment</option>
-                    <option value='support'>Support</option>
-                </select>
-                <button type="submit" >Add Queue</button>
-            </form>
-        </>
-    )
-}
+  return (
+    <div className="p-5 mt-5 bg-gray-800 text-white rounded">
+      <div className="text-xl mb-4">Queue Form</div>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+        <input
+          className="p-2 h-11 bg-gray-700 text-white border border-gray-600 rounded"
+          type="input"
+          value={customer}
+          onChange={(e) => setCustomer(e.target.value)}
+          placeholder="Customer Name"
+        />
+        <select
+          className="p-2 h-11 bg-gray-700 text-white border border-gray-600 rounded"
+          value={service}
+          onChange={(e) => setService(e.target.value)}
+        >
+          <option value="">Select Service</option>
+          <option value="consultation">Consultation</option>
+          <option value="payment">Payment</option>
+          <option value="support">Support</option>
+        </select>
+        <button
+          className="p-2 mt-3 h-11 bg-amber-500 text-black rounded hover:bg-amber-600 hover:text-gray-200 cursor-pointer transition-all"
+          type="submit"
+        >
+          Add Queue
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddQueueForm;
--- src/components/DisplayQueue.jsx
+++ src/components/DisplayQueue.jsx
@@ -1,26 +1,51 @@
 const DisplayQueue = ({ queueList, updateStatus, removeFromQueue }) => {
-    return queueList.length <= 0 ? 'No Queue Yet' : (
-        <>
-            <div><b>List of Queues</b></div>
-            {queueList.map((queue) => {
-                const { id, customer, service, status } = queue
-                return (
-                    <div key={id}>
-                        <div>Name: {customer}</div>
-                        <div>Service: {service}</div>
-                        <div>Status: {status}</div>
-                        <button onClick={() => updateStatus(id, 'in-progress')}>
-                            Update
-                        </button>
-                        <button onClick={() => removeFromQueue(id)}>
-                            Delete
-                        </button>
-                        <hr />
-                    </div>
-                )
-            })}
-        </>
-    )
-}
+  return queueList.length <= 0 ? (
+    <div className="w-full p-5 mt-5 bg-gray-800 text-white text-xl flex justify-center items-center rounded">
+      Empty Queue
+    </div>
+  ) : (
+    <div className="w-full p-5 mt-5 bg-gray-800 text-white rounded">
+      <div className="text-xl mb-4 text-white border-b-3 border-gray-600 p-2 rounded">List of Queues</div>
+      {queueList.map((queue) => {
+        const { id, customer, service, status } = queue;
+        return (
+          <div key={id} className="flex justify-between items-center border-b border-gray-600 py-2">
+            <div>
+              <div>Name: {customer}</div>
+              <div>
+                Service:{" "}
+                {service
+                  .split("")
+                  .map((s, i) => (i === 0 ? s.toUpperCase() : s))
+                  ?.join("")}
+              </div>
+              <div>
+                Status:{" "}
+                {status
+                  ?.split("")
+                  .map((s, i) => (i === 0 ? s.toUpperCase() : s))
+                  ?.join("")}
+              </div>
+            </div>
+            <div>
+              <button
+                className="p-2 h-11 bg-amber-500 text-black rounded hover:bg-amber-600 hover:text-gray-200 cursor-pointer transition-all"
+                onClick={() => updateStatus(id, "in-progress")}
+              >
+                Update
+              </button>
+              <button
+                className="p-2 m-4 h-11 bg-red-500 text-black rounded hover:bg-red-800 hover:text-gray-200 cursor-pointer transition-all"
+                onClick={() => removeFromQueue(id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default DisplayQueue;
