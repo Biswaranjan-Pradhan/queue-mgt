6892614178915f3e329e888046683ad36fb3ad24
Biswaranjan Pradhan <biswaranjan.pradhan@fiserv.com>
2026-09-25 23:27:06 +0530

    Fixed completed queue and added mobile device style

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,47 @@ import AddQueueForm from './components/AddQueueForm.jsx'
 import DisplayQueue from './components/DisplayQueue.jsx'
 
 const App = () => {
-    const [queue, setQueue] = useState([])
+    const [queue, setQueue] = useState([]);
+    const [completedQueue, setCompletedQueue] = useState([]);
+
 
     const addQueue = (customer) => {
-        setQueue([...queue, { ...customer, id: Date.now(), status: 'waiting' }])
+        setQueue([...queue, { ...customer, id: Date.now(), status: 'waiting' }]);
     }
 
     const updateStatus = (id, newStatus) => {
+        const customer = queue.find((customer) => customer.id === id);
+
+        if (newStatus === "completed" && customer) {
+            setCompletedQueue((completedQueue) => [
+                ...completedQueue,
+                { ...customer, status: newStatus },
+            ]);
+            removeFromQueue(id);
+            return;
+        }
+
         setQueue(
             queue.map((customer) =>
                 customer.id === id ? { ...customer, status: newStatus } : customer
             )
-        )
+        );
     }
 
     const removeFromQueue = (id) => {
         setQueue(queue.filter((customer) => customer.id !== id))
     }
 
-
+    console.log("Queue",queue);
+    console.log("completedQueue",completedQueue);
     return (
         <div className="min-h-screen flex flex-col bg-black">
             <div>
-                <h2 className="text-3xl text-amber-500 pt-2 px-4">Queue Management System</h2>
+                <h2 className="text-2xl md:text-3xl text-center md:text-left text-amber-500 pt-2 px-4">Queue Management System</h2>
             </div>
-            <div className="flex justify-between gap-4">
+            <div className="flex flex-col md:flex-row justify-between gap-4">
                 <AddQueueForm onAdd={addQueue}/>
-                <DisplayQueue queueList={queue} updateStatus={updateStatus} removeFromQueue={removeFromQueue}/>
+                <DisplayQueue completedQueue={completedQueue} queueList={queue} updateStatus={updateStatus} removeFromQueue={removeFromQueue}/>
             </div>
         </div>
     )   
--- src/components/DisplayQueue.jsx
+++ src/components/DisplayQueue.jsx
@@ -1,49 +1,141 @@
-const DisplayQueue = ({ queueList, updateStatus, removeFromQueue }) => {
-  return queueList.length <= 0 ? (
+const DisplayQueue = ({
+  queueList,
+  updateStatus,
+  removeFromQueue,
+  completedQueue,
+}) => {
+  const getStatusColor = (status) => {
+    switch (status) {
+      case "waiting":
+        return "text-amber-500";
+      case "serving":
+        return "text-green-500";
+      case "completed":
+        return "text-red-500";
+      default:
+        return "text-black";
+    }
+  };
+
+  return queueList.length <= 0 && completedQueue.length <= 0 ? (
     <div className="w-full p-5 mt-5 bg-gray-800 text-white text-xl flex justify-center items-center rounded">
       Empty Queue
     </div>
   ) : (
-    <div className="w-full p-5 mt-5 bg-gray-800 text-white rounded">
-      <div className="text-xl mb-4 text-white border-b-3 border-gray-600 p-2 rounded">List of Queues</div>
-      {queueList.map((queue) => {
-        const { id, customer, service, status } = queue;
-        return (
-          <div key={id} className="flex justify-between items-center border-b border-gray-600 py-2">
-            <div>
-              <div>Name: {customer}</div>
-              <div>
-                Service:{" "}
-                {service
-                  .split("")
-                  .map((s, i) => (i === 0 ? s.toUpperCase() : s))
-                  ?.join("")}
-              </div>
-              <div>
-                Status:{" "}
-                {status
-                  ?.split("")
-                  .map((s, i) => (i === 0 ? s.toUpperCase() : s))
-                  ?.join("")}
-              </div>
-            </div>
-            <div>
-              <button
-                className="p-2 h-11 bg-amber-500 text-black rounded hover:bg-amber-600 hover:text-gray-200 cursor-pointer transition-all"
-                onClick={() => updateStatus(id, "in-progress")}
+    <div className="w-full flex flex-col">
+      <div className="w-full p-5 mt-5 bg-gray-800 text-white rounded">
+        <div className="text-xl mb-1 text-green-500 border-b-3 border-gray-600 p-2 rounded">
+          Active Queues ({queueList.length})
+        </div>
+        {queueList.length === 0 ? (
+          <div className="text-center w-full">No Active Queues</div>
+        ) : (
+          queueList.map((queue, index) => {
+            const { id, customer, service, status } = queue;
+            return (
+              <div
+                key={id}
+                className="flex justify-start items-center rounded border-b border-gray-600 py-2 px-2 hover:bg-gray-600 transition-all"
               >
-                Update
-              </button>
-              <button
-                className="p-2 m-4 h-11 bg-red-500 text-black rounded hover:bg-red-800 hover:text-gray-200 cursor-pointer transition-all"
-                onClick={() => removeFromQueue(id)}
-              >
-                Delete
-              </button>
-            </div>
+                <div className="relative w-10 h-10 bg-amber-500 rounded-3xl">
+                  <div className="px-4 pt-2">{index + 1}</div>
+                </div>
+                <div className=" w-50 ml-5">
+                  <div>Name: {customer}</div>
+                  <div>
+                    Service:&nbsp;
+                    {service
+                      .split("")
+                      .map((s, i) => (i === 0 ? s.toUpperCase() : s))
+                      ?.join("")}
+                  </div>
+                  <div>
+                    Status:&nbsp;
+                    <span className={getStatusColor(status)}>
+                      {status
+                        ?.split("")
+                        .map((s, i) => (i === 0 ? s.toUpperCase() : s))
+                        ?.join("")}
+                    </span>
+                  </div>
+                </div>
+                <div className="ml-auto">
+                  {status === "waiting" && (
+                    <button
+                      className="p-2 ml-4 sm:h-8 md:h-11 bg-amber-500 text-black rounded hover:bg-amber-600 hover:text-gray-200 cursor-pointer transition-all"
+                      onClick={() => updateStatus(id, "serving")}
+                    >
+                      Serve
+                    </button>
+                  )}
+                  {status === "serving" && (
+                    <button
+                      className="p-2 md:h-11 bg-amber-500 text-black rounded hover:bg-amber-600 hover:text-gray-200 cursor-pointer transition-all"
+                      onClick={() => updateStatus(id, "completed")}
+                    >
+                      Complete
+                    </button>
+                  )}
+                  {status !== "serving" && (
+                    <button
+                      className="p-2 m-4 md:h-11 bg-red-500 text-black rounded hover:bg-red-800 hover:text-gray-200 cursor-pointer transition-all"
+                      onClick={() => removeFromQueue(id)}
+                    >
+                      Delete
+                    </button>
+                  )}
+                </div>
+              </div>
+            );
+          })
+        )}
+      </div>
+      <div className="w-full p-5 mt-5 bg-gray-800 text-white rounded">
+        <div className="text-xl text-red-500 border-b-3 border-gray-600 mb-1 p-2 rounded">
+          Completed Queues ({completedQueue.length})
+        </div>
+        {completedQueue.length === 0 ? (
+          <div className="flex justify-between items-center py-2">
+            <div className="text-center w-full">No Completed Queues</div>
           </div>
-        );
-      })}
+        ) : (
+          completedQueue.map((completedQueue, index) => {
+            const { id, customer, service, status } = completedQueue;
+            if (status === "completed") {
+              return (
+                <div
+                  key={id}
+                  className="flex justify-start items-center border-b border-gray-600 rounded p-4 hover:bg-gray-600"
+                >
+                  <div className="relative w-10 h-10 bg-red-500 rounded-3xl">
+                    <div className="px-4 pt-2">{index + 1}</div>
+                  </div>
+                  <div className="flex flex-col ml-4 md:flex-row md:ml-5">
+                    <div className="md:ml-4 md:mr-8">Name: {customer}</div>
+                    <div className="md:mr-8">
+                      Service:&nbsp;
+                      {service
+                        .split("")
+                        .map((s, i) => (i === 0 ? s.toUpperCase() : s))
+                        ?.join("")}
+                    </div>
+                    <div className="md:mr-8">
+                      Status:&nbsp;
+                      <span className={getStatusColor(status)}>
+                        {status
+                          ?.split("")
+                          .map((s, i) => (i === 0 ? s.toUpperCase() : s))
+                          ?.join("")}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            }
+            return null;
+          })
+        )}
+      </div>
     </div>
   );
 };
